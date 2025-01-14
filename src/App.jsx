72196e5efb6bb2c6b8aa66fae18b12801dfe6bc1
Axel Denouly <axeljoo2935@gmail.com>
2025-01-14 09:26:43 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import UpdateTimeButton from "./Components/UpdateTimeButton"
+import ToggleButton from "./Components/ToggleButton"
+import { useSelector } from "react-redux"
+import getFormatedValue from "./utils/getFormatedValue"
+
+function App() {
+
+  const chronoValues = useSelector(state => state.chrono)
+
+  return (
+    <div className="bg-slate-700 text-slate-100 pt-20 min-h-screen">
+      <div className="max-w-xl mx-auto border border-slate-600 rounded p-10">
+        <h1 className="text-center text-3xl mb-2">Pomodoro App</h1>
+        <p className="text-center text-xm mb-8 text-slate-400">Set your concentration and break time.</p>
+
+        <div className="flex justify-center mb-8">
+
+          {/* Session Block */}
+          <div className="mr-10">
+            <p className="text-center mb-1">Sessions</p>
+            <div className="flex">
+              <UpdateTimeButton sign={"-"} type={"session"} />
+              <p className="mx-5 text-xl">{chronoValues.session.value / 60} </p>
+              <UpdateTimeButton sign={"+"} type={"session"} />
+            </div>
+          </div>
+
+          {/* Pause Block */}
+          <div>
+            <p className="text-center mb-1">Pauses</p>
+            <div className="flex">
+              <UpdateTimeButton sign={"-"} type={"pause"} />
+              <p className="mx-5 text-xl">{chronoValues.pause.value  / 60}</p>
+              <UpdateTimeButton sign={"+"} type={"pause"} />
+
+            </div>
+          </div>
+
+        </div>
+
+        <p className="text-center mb-2 text-xl font-semibold">{chronoValues.displayedValue.heading}</p>
+
+        <p className="text-center flex justify-center mb-1">
+          <span className="text-4xl py-2 px-24 rounded bg-slate-300 text-slate-900">{getFormatedValue(chronoValues.displayedValue.value)}</span>
+        </p>
+        <p className="mb-10 text-center">Passed cycle(s) : {chronoValues.cycles}</p>
+
+        <ToggleButton />
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
